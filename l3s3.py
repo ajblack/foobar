d399e74436129d9fbf806cd8eb009deb8c0cf625
Austin Blackman <austin.blackman@gmail.com>
2018-03-09 19:59:41 -0500

    it works, but too slow

--- l3s3.py
+++ l3s3.py
@@ -13,32 +13,34 @@ def answer(l):
     numMatches = 0
     listLength = len(l)
 
-
-
-
     p1Index=0
     p2Index=1
     p3Index=2
 
 
-    while p1Index < 1+len(l)-3:
-        while p2Index < 1+len(l)-2:
+    #while p1Index < len(l)-2:
+    while len(l) > 0:
+        print("moving P1")
+        while p2Index < len(l)-1:
             while p3Index < len(l):
                 if l[p3Index]%l[p2Index]==0 and l[p2Index]%l[p1Index]==0:
-                    print('match found')
-                    p="p1 is: %s, p2 is: %s, p3 is: %s"%(l[p1Index],l[p2Index],l[p3Index])
+                    p="match found with P1: %s, P2: %s, P3: %s"%(l[p1Index], l[p2Index], l[p3Index])
                     print(p)
                     numMatches+=1
-                p3Index = findNextMultipleIndex(l,p2Index, p3Index)
+                p3Index = findNextMultipleIndex(l, p2Index, p3Index)
             p2Index=findNextMultipleIndex(l, p1Index, p2Index)
-            if p2Index < listLength:
-                p3Index=findNextMultipleIndex(l, p2Index, p2Index)
-        p1Index+=1
-        p2Index=findNextMultipleIndex(l, p1Index, p2Index)
-        p3Index=findNextMultipleIndex(l, p2Index, p2Index)
+            p3Index=findNextMultipleIndex(l, p2Index, p2Index)
+        #p1Index+=1
+        #p2Index=findNextMultipleIndex(l, p1Index, p2Index)
+        #p3Index=findNextMultipleIndex(l, p2Index, p2Index
+
+        l.pop(0)
+        p1Index=0
+        p2Index=findNextMultipleIndex(l,p1Index,p1Index)
+        p3Index=findNextMultipleIndex(l,p2Index,p2Index)
     return numMatches
 
 
-l1 =[5,12,20,25,100,112,150]
+l1 =[5,12,20,25,100,112,200]
 v = answer(l1)
 print(v)
