bb1b5df8e212e246cce1b69072f3e8ef22214f25
Austin Blackman <austin.blackman@gmail.com>
2018-03-10 14:41:50 -0500

    I think this is my fastest yet, an optimized O(n^2)

--- l3s3.py
+++ l3s3.py
@@ -1,6 +1,8 @@
+
 def answer(l):
     myset = set()
-    lset = set()
+    retset = set()
+    l.sort()
     sumMatches = 0
     outerIndex=1
     #for v in l:
@@ -10,24 +12,44 @@ def answer(l):
                 myset.add((l[v],l[innerV],innerV))
         outerIndex +=1
     print(myset)
-
-    listIndex = 0
-    for listVal in l:
-        for v1,v2,index in myset:
-            if  listVal >= v2 and  listVal%v2==0 and listIndex>index:
+    print(l)
+    for myV1, myV2, myV3 in myset:
+        #for listVal in l[mytuple[2]+1:]:
+        for listVal in range(myV3+1, len(l)):
+            if  l[listVal]%myV2==0:
+                p="listVal is %s and mytuple[1] is %s"%(l[listVal],myV2)
+                print(p)
+                retset.add((myV1,myV2,l[listVal]))
                 sumMatches+=1
-        listIndex+=1
-    return sumMatches
+    return len(retset)
+'''
 
+def answer(l):
+    mydict = {}
+    l.sort()
+    sumMatches = 0
+    outerIndex=1
+    #for v in l:
+    for v in range(0,len(l)-2):
+        for innerV in range(outerIndex,len(l)-1):
+            if l[innerV]%l[v]==0:
+                mydict[(l[v],l[innerV])] = innerV
+        outerIndex +=1
+    print(mydict)
+    for key, value in mydict.items():
+        #for listVal in l[mytuple[2]+1:]:
+        for listVal in range(value+1, len(l)):
+            if  l[listVal]%key[1]==0:
+                p="listVal is %s and mytuple[1] is %s"%(l[listVal],key[1])
+                print(p)
+                sumMatches+=1
+    return sumMatches
+'''
 
 l1 =[5,12,20,25,100,112,120,200,240]
-l2 =[5,12,20,25,100,112,200]
-v=answer(l1)
-print(v)
-c=answer(l2)
-print(c)
 
-
-l3=[1,1,1]
+l3 =[1,2,3,3,3,4,5,6]
+#v=answer(l1)
+#print(v)
 c=answer(l3)
 print(c)
