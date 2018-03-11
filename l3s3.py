7e1a30f17be44d20ab04853f2e24192aef241a89
Austin Blackman <austin.blackman@gmail.com>
2018-03-10 22:21:31 -0500

    removed sort, using arrays

--- l3s3.py
+++ l3s3.py
@@ -2,7 +2,6 @@
 def answer(l):
     myset = set()
     retset = set()
-    l.sort()
     sumMatches = 0
     outerIndex=1
     #for v in l:
@@ -22,34 +21,35 @@ def answer(l):
                 retset.add((myV1,myV2,l[listVal]))
                 sumMatches+=1
     return len(retset)
-'''
 
+
+'''
 def answer(l):
-    mydict = {}
-    l.sort()
-    sumMatches = 0
+    #myset = set()
+    mylist = []
+    retlist = []
     outerIndex=1
+    numMatches = 0
     #for v in l:
     for v in range(0,len(l)-2):
         for innerV in range(outerIndex,len(l)-1):
             if l[innerV]%l[v]==0:
-                mydict[(l[v],l[innerV])] = innerV
+                #myset.add((l[v],l[innerV],innerV))
+                mylist.append((l[v],l[innerV],innerV))
         outerIndex +=1
-    print(mydict)
-    for key, value in mydict.items():
+    print(mylist)
+    for v in range(0,len(mylist)):
         #for listVal in l[mytuple[2]+1:]:
-        for listVal in range(value+1, len(l)):
-            if  l[listVal]%key[1]==0:
-                p="listVal is %s and mytuple[1] is %s"%(l[listVal],key[1])
-                print(p)
-                sumMatches+=1
-    return sumMatches
+        for listVal in range(mylist[v][2]+1, len(l)):
+                #retlist.append((myV1,myV2,l[listVal]))
+                if l[listVal]%mylist[v][1]==0:
+                    numMatches+=1
+    return numMatches
 '''
-
 l1 =[5,12,20,25,100,112,120,200,240]
 
 l3 =[1,2,3,3,3,4,5,6]
-#v=answer(l1)
-#print(v)
+v=answer(l1)
+print(v)
 c=answer(l3)
 print(c)
